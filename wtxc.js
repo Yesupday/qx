e9db4638f432d714e023e4e5fd6542cf54a771de
xwh <77>
2025-06-17 22:48:43 +0800

    1

--- wtxc.js
+++ wtxc.js
@@ -1,7 +1,6 @@
-// https://raw.githubusercontent.com/Yesupday/qx/refs/heads/master/szjjmxx.js
 // # 哈哈替换车牌默认值
 // [rewrite_local]
-// ^https:\/\/gzh\.szjj\.sz\.gov\.cn\/h5\/static\/js\/54\..*\.js$ url script-response-body https://raw.githubusercontent.com/YourUsername/YourRepo/main/wtxc.js
+// ^https:\/\/gzh\.szjj\.sz\.gov\.cn\/h5\/static\/js\/54\..*\.js$ url script-response-body https://github.com/Yesupday/qx/raw/refs/heads/master/wtxc.js
 // [mitm]
 // hostname = gzh.szjj.sz.gov.cn
 
